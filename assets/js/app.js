dafbaf6c4c4b6d95c0e904ca9dd06befbf50cbcb
Diwas Paudel <diwasp1@gmail.com>
2020-11-11 01:50:25 +0545

    search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,47 @@
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  } 
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
-  var btn = $('#button');
+let searchIcon = document.getElementById("search-icon");
+let searchBar = document.querySelector(".search-bar");
+let hamBar = document.querySelector("#ham-bar");
 
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
+searchIcon.addEventListener("click", displaySearch);
+
+function displaySearch(e) {
+  e.preventDefault();
+  if (searchBar.style.display == "none") {
+    searchBar.style.display = "inline-block";
+    // searchBar.style.transition = "all .9s linear";
+    searchBar.focus();
+    searchBar.value = "";
   } else {
-    btn.removeClass('show');
+    searchBar.style.display = "none";
+  }
+}
+window.onclick = function (event) {
+  if (event.target != searchBar && event.target != searchIcon) {
+    searchBar.style.display = "none";
+  }
+};
+
+var btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
   }
 });
 
-btn.on('click', function(e) {
+btn.on("click", function (e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $("html, body").animate({ scrollTop: 0 }, "300");
 });
